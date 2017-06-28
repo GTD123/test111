6a454b5062bf10b6bca1fb85bcd29b0614692f33
RQ <791538998@qq.com>
2017-06-28 09:31:20 +0800

    我 提交了

--- src/com/example/hello/Entity/Person.java
+++ src/com/example/hello/Entity/Person.java
@@ -9,38 +9,5 @@ public class Person {
     private int age;
     private boolean married;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public boolean isMarried() {
-        return married;
-    }
-
-    public void setMarried(boolean married) {
-        this.married = married;
-    }
-
-
 }
 
